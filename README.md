e236d18b4a6e7379899bcf17da24bd65b9f995fa
Dragon44PL <77531351+Dragon44PL@users.noreply.github.com>
2021-07-13 15:58:51 +0200

    feature/login (#1)
    * Created Login Component template
    
    * Added logo, changed main colour, variables name and padding
    
    * Changed logo
    
    * Changed name to Musiva, added styles for 768px
    
    * Added transition to buttons
    
    * Renamed project to Musiva

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DratifyWeb
+# Musiva-Web
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0.
 
---
+++ src/app/core/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  title = "Aby kontynuować, zaloguj się do Musiva"
+  loginTitle = "Adres e-mail lub nazwa użytkownika"
+  passwordTitle = "Hasło"
+  passwordReset = "Nie pamiętasz hasła?"
+  rememberMe = "Zapamiętaj mnie"
+  signIn = "Zaloguj się"
+  signUpTitle = "Nie masz jeszcze konta?"
+  signUp = "Zarejestruj się w Musiva"
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
