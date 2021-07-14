2afd98b0c39a829ccf48ee971bf9848bf4e072d7
Dragon44PL <krystian.pieniak44@gmail.com>
2021-07-14 13:44:49 +0200

    Created register structure

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  signUpOAuthTitle = "Aby kontynuować, zaloguj się do Spotify.";
+  signUpOAuthGoogle = "Zarejestruj się przez Google";
+
+  signUpTitle = "Zarejestruj się za pomocą adresu e-mail";
+  signUpMail = "Twój adres e-mail";
+  signUpMailConfirm = "Potwierdź swój e-mail";
+  signUpPassword = "Stwórz hasło";
+  signUpLogin = "Jak mamy się do Ciebie zwracać?";
+  signUpBirthDate = "Podaj swoją datę urodzenia";
+  signUpGender = "Twoja płeć?";
+  signUpOffers = "Chcę otrzymywać wiadomości i oferty od Spotify";
+  signUpGenderMale = "Mężczyzna";
+  signUpGenderFemale = "Kobieta";
+  signUpGenderOther = "Inne";
+  signUpButton = "";
+  signInTitle = "";
+  signInLink = "Zaloguj się";
+
   constructor() { }
 
   ngOnInit(): void {
