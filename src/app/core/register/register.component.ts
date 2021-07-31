ba420386e6a77fa7ff19a2a17b2e9c0aed356953
Dragon44PL <77531351+Dragon44PL@users.noreply.github.com>
2021-07-31 18:05:43 +0200

    feature/register (#6)
    * Created register component
    
    * Created register structure
    
    * Updated styles
    
    * Updated styles for structure
    
    * Updated for 768px width

---
+++ src/app/core/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  signUpOAuthTitle = "Zarejestruj się za darmo i zacznij słuchać.";
+  signUpOAuthGoogle = "Kontynuuj z Google";
+
+  signUpTitle = "Zarejestruj się za pomocą adresu e-mail";
+  signUpMail = "Twój adres e-mail";
+  signUpMailConfirm = "Potwierdź swój e-mail";
+  signUpPassword = "Stwórz hasło";
+  signUpLogin = "Jak mamy się do Ciebie zwracać?";
+  signUpBirthDate = "Podaj swoją datę urodzenia";
+  signUpBirthDateDay = "Dzień";
+  signUpBirthDateDayShort = "DD";
+  signUpBirthDateMonth = "Miesiąc";
+  signUpBirthDateYear = "Rok";
+  signUpBirthDateYearShort = "YYYY";
+  signUpGender = "Twoja płeć?";
+  signUpOffers = "Chcę otrzymywać wiadomości i oferty od Musiva";
+  signUpGenderMale = "Mężczyzna";
+  signUpGenderFemale = "Kobieta";
+  signUpGenderOther = "Osoba niebinarna";
+  signUpButton = "Zarejestruj się";
+  signInTitle = "Masz już konto?";
+  signInLink = "Zaloguj się.";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
